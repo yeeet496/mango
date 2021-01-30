4267abaa455d83421e1912627665839b0178b973
yeeet496 <75123149+yeeet496@users.noreply.github.com>
2021-01-30 11:38:06 +0530

    Add files via upload

---
+++ stone.js
@@ -0,0 +1,14 @@
+class Stone{
+constructor(bodyA,bodyB){
+    var options={
+   isStatic: false,
+    restitution: 0,
+    friction:1,
+    density:1.2,
+    }
+   
+}
+}
+
+display();
+mango1.display();
